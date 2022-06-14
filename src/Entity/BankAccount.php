63ca27e2e083e9098880ec399d4055e5f01c9028
Moufid17 <moufid.mtr@gmail.com>
2022-06-14 15:41:52 +0200

    Init entities

---
+++ src/Entity/BankAccount.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\BankAccountRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: BankAccountRepository::class)]
+class BankAccount
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'integer')]
+    private $balance;
+
+    #[ORM\OneToOne(mappedBy: 'account', targetEntity: User::class, cascade: ['persist', 'remove'])]
+    private $owner;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getBalance(): ?int
+    {
+        return $this->balance;
+    }
+
+    public function setBalance(int $balance): self
+    {
+        $this->balance = $balance;
+
+        return $this;
+    }
+
+    public function getOwner(): ?User
+    {
+        return $this->owner;
+    }
+
+    public function setOwner(?User $owner): self
+    {
+        // unset the owning side of the relation if necessary
+        if ($owner === null && $this->owner !== null) {
+            $this->owner->setAccount(null);
+        }
+
+        // set the owning side of the relation if necessary
+        if ($owner !== null && $owner->getAccount() !== $this) {
+            $owner->setAccount($this);
+        }
+
+        $this->owner = $owner;
+
+        return $this;
+    }
+}
